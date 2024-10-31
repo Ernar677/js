58e60e4eb1b60b34789b0ac0343b1028c29aa6de
Ernar <ernartat2009@gmail.com>
2024-10-31 22:15:12 +0500

    УЙ ТАПСЫРМАСЫ ДАЙЫН!

---
+++ hw2/hww.js
@@ -0,0 +1,8 @@
+
+let firstNumber = parseFloat(prompt("Бірінші санды енгізіңіз:"));
+let secondNumber = parseFloat(prompt("Екінші санды енгізіңіз:"));
+let thirdNumber = parseFloat(prompt("Үшінші санды енгізіңіз:"));
+
+let arithmeticMean = (firstNumber + secondNumber + thirdNumber) / 3;
+
+alert("Енгізілген сандардың орташа арифметикалық мәні: " + arithmeticMean);
