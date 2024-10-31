ae3a6106cc9846eedae2931bb0234fb67529b856
Ernar <ernartat2009@gmail.com>
2024-10-31 15:55:11 +0500

    Сделал домашнию работу

---
+++ script.js
@@ -0,0 +1,10 @@
+let name = "Ali"; 
+let age = 25; 
+let isStudent = true;
+
+console.log("аты:", name, "| Тип:", typeof name);
+console.log("Жасы:", age, "| Тип:", typeof age);
+console.log("Студент?:", isStudent, "| Тип:", typeof isStudent);
+
+let ageAsString = age.toString(); 
+console.log("Жасы (как строка):", ageAsString, "| Тип:", typeof ageAsString); 
