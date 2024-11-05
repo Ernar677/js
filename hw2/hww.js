e2f92524a163c0415de9868a1be3db4e3e06e66f
Ernar <ernartat2009@gmail.com>
2024-11-06 00:35:11 +0500

    hw4 1 тапсырмасы орындалды!!

--- hw2/hww.js
+++ hw2/hww.js
@@ -3,6 +3,6 @@ let Number1 = Number(prompt("Бірінші санды енгізіңіз:"));
 let Number2 = Number(prompt("Екінші санды енгізіңіз:"));
 let Number3 = Number(prompt("Үшінші санды енгізіңіз:"));
 
-let arithmetic = (Number1 + Number2 + Number3) / 3;
+let arithmetic = (Number1 + Number2 + Number3) /3;
 
 alert("Енгізілген сандардың орташа арифметикалық мәні: " + arithmetic);
---
+++ hw3/script.js
@@ -0,0 +1,54 @@
+// let age = prompt("SAN ENGIZ");
+// let isStudent = false;
+// if(age>=65){
+//     alert(true)
+// }
+// else{
+// alert(false)
+// }
+
+// task2
+
+// let age = prompt("San engiz")
+// if(age<=20 && age>=10){
+//     alert(true)
+// }
+// else{
+//     alert(false)
+// }
+
+
+//task 3
+// let num1 = -6
+// let num2 = 10
+
+// console.log(num1<=0 && num2>=0)|| (num1);
+
+
+
+
+
+// //task2.2
+// let age = prompt("Жасын нешеде?")
+// let isStudent = false
+
+// if(age>=65 || isStudent==true){
+//     alert("Сізге акция беріледі!")
+// } 
+// else{
+//     alert("Сізге акция жүргізлмейді!")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
